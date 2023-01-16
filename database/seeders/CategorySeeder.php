0abf11a60ebe33d100a9fec359610aa1472812c6
MicheleRossetti02 <michelerossetti07@gmail.com>
2023-01-16 18:36:35 +0100

    migration

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['hip-pop', 'rap', 'cantautorato', 'latino', 'dance',];
+
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->slug = Str::generateSlag($newCategory->name);
+            $newCategory->save();
+        }
+    }
+}
